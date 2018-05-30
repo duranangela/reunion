2cf8b1fb30243f9e24ece718ca5d45dd33d5f91f
Angela Duran <duranangela19@gmail.com>
2018-05-30 10:01:33 -0600

    Added activity functionality and corresponding tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,33 @@
 class Activity
+  attr_reader :name, :base_cost, :cost_per
+  attr_accessor :participants
+
+  def initialize(name, base_cost, cost_per)
+    @name = name
+    @participants = Hash.new
+    @base_cost = base_cost
+    @cost_per = cost_per
+  end
+
+  def add_participants(participants)
+    participants.each do |person, amt_paid|
+      @participants[person] = amt_paid
+    end
+  end
+
+  def total_cost
+    participant_cost = @participants.keys.count * @cost_per
+    total_cost = participant_cost + @base_cost
+  end
+
+  def total_cost_per
+    total_cost / @participants.keys.count
+  end
+
+  def totals_owed
+    @participants.each do |person, amt_paid|
+      @participants[person] = total_cost_per - amt_paid
+    end
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,42 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_has_attributes
+    a = Activity.new("Volleyball", 10, 5)
+    assert_instance_of Activity, a
+    assert_equal "Volleyball", a.name
+    assert_equal 10, a.base_cost
+    assert_equal 5, a.cost_per
+  end
+
+  def test_it_starts_with_no_participants
+    a = Activity.new("Volleyball", 10, 5)
+    assert_equal Hash.new, a.participants
+  end
+
+  def test_it_can_add_participants
+    a = Activity.new("Volleyball", 10, 5)
+    a.add_participants({'Bob' => 5, 'Alice' => 4})
+    assert_equal ({'Bob' => 5, 'Alice' => 4}), a.participants
+  end
+
+  def test_it_can_calculate_total_cost
+    a = Activity.new("Volleyball", 6, 5)
+    a.add_participants({'Bob' => 5, 'Alice' => 4, 'Fred' => 7})
+    assert_equal 21, a.total_cost
+  end
+
+  def test_ic_can_calculate_total_cost_per_person
+    a = Activity.new("Volleyball", 6, 5)
+    a.add_participants({'Bob' => 5, 'Alice' => 4, 'Fred' => 7})
+    assert_equal 7, a.total_cost_per
+  end
+
+  def test_it_can_calculate_total_owed_by_each_participant
+    a = Activity.new("Volleyball", 6, 5)
+    a.add_participants({'Bob' => 5, 'Alice' => 4, 'Fred' => 7})
+    assert_equal ({'Bob' => 2, 'Alice' => 3, 'Fred' => 0}), a.totals_owed
+  end
+
 end
