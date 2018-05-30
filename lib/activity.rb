5c2930298599b2905090e419956ca8d524ce67a0
Angela Duran <duranangela19@gmail.com>
2018-05-30 10:44:33 -0600

    Trying something with hashes...

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,33 +1,34 @@
 class Activity
-  attr_reader :name, :base_cost, :cost_per
-  attr_accessor :participants
+  attr_accessor :activity
 
   def initialize(name, base_cost, cost_per)
-    @name = name
-    @participants = Hash.new
-    @base_cost = base_cost
-    @cost_per = cost_per
+    @activity = {name: name,
+                base_cost: base_cost,
+                cost_per: cost_per,
+                participants: Hash.new
+                }
   end
+
 
   def add_participants(participants)
     participants.each do |person, amt_paid|
-      @participants[person] = amt_paid
+      @activity[participants[person]] = amt_paid
     end
   end
 
-  def total_cost
-    participant_cost = @participants.keys.count * @cost_per
-    participant_cost + @base_cost
-  end
-
-  def total_cost_per
-    total_cost / @participants.keys.count
-  end
-
-  def totals_owed
-    @participants.each do |person, amt_paid|
-      @participants[person] = total_cost_per - amt_paid
-    end
-  end
+  # def total_cost
+  #   participant_cost = @participants.keys.count * @cost_per
+  #   participant_cost + @base_cost
+  # end
+  #
+  # def total_cost_per
+  #   total_cost / @participants.keys.count
+  # end
+  #
+  # def totals_owed
+  #   @participants.each do |person, amt_paid|
+  #     @participants[person] = total_cost_per - amt_paid
+  #   end
+  # end
 
 end
